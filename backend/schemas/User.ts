20862a6d28d9cbdc379e247a317905b475c041bd
Yang-Yi-Shen <mrshenyangyi@gmail.com>
2025-02-24 15:45:50 +0800

    fix(backend): make linklists plural in user schema

--- backend/schemas/User.ts
+++ backend/schemas/User.ts
@@ -4,7 +4,7 @@ export interface UserData {
     // ids are management by firebase via the push id system
     name: string,
     password: string,
-    linkList: {
+    linkLists: {
         [key: string]: LinkListData
     },
 }
